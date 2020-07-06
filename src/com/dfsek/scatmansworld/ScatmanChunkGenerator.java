5c94cd9215d97ffefd20db1e99de58c084a2fd42
dfsek <dfsek@protonmail.com>
2020-07-06 16:53:40 -0700

    disabled caves bc SPIGOT-5880 strikes again.

--- src/com/dfsek/scatmansworld/ScatmanChunkGenerator.java
+++ src/com/dfsek/scatmansworld/ScatmanChunkGenerator.java
@@ -2,6 +2,7 @@ package com.dfsek.scatmansworld;
 
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.util.noise.SimplexOctaveGenerator;
 
@@ -17,6 +18,7 @@ public class ScatmanChunkGenerator extends ChunkGenerator {
         canyonNoise.setScale(0.0075);
         for(int x = 0; x < 16; x++) {
             for(int z = 0; z < 16; z++) {
+                biome.setBiome(x, z, Biome.PLAINS);
                 int h = (int) (((noise.noise(chunkX*16 + x, chunkZ * 16 + z, 0.5D, 0.5D, true)
                         + noise.noise((chunkX*16 + x)*2, (chunkZ * 16 + z)*2, 0.5D, 0.5D, true)*0.5
                         + noise.noise((chunkX*16 + x)*4, (chunkZ * 16 + z)*4, 0.5D, 0.5D, true)*0.25
@@ -25,7 +27,7 @@ public class ScatmanChunkGenerator extends ChunkGenerator {
                 if(absNoise < 0.04) h = (h-54)/5 + 34;
                 for(int y = 0; y < h; y++) {
                     if(y > h-3) chunk.setBlock(x, y, z, Material.RED_SAND);
-                    else if(y > h-6 || (absNoise < 0.06 && absNoise > 0.04)) chunk.setBlock(x, y, z, Material.ORANGE_TERRACOTTA);
+                    else if(y > h-6 || (absNoise < 0.07 && absNoise > 0.04)) chunk.setBlock(x, y, z, Material.ORANGE_TERRACOTTA);
                     else chunk.setBlock(x, y, z, Material.STONE);
                 }
                 if(random.nextInt(100) < 50) chunk.setBlock(x,1,z,Material.BEDROCK);
@@ -37,10 +39,6 @@ public class ScatmanChunkGenerator extends ChunkGenerator {
         }
         chunk.setRegion(0, 0, 0, 16, 1, 16, Material.BEDROCK);
         return chunk;
-    }
-    @Override
-    public boolean shouldGenerateCaves() {
-        return true;
     }
     @Override
     public boolean shouldGenerateDecorations() {
